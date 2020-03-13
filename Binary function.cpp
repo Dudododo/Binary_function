ba99bbe025f0a108d9ca0b2a2a97892fa11ffc11
Dudododo <a752601260@gmail.com>
2020-03-13 14:27:30 +0800

    添加项目文件。

---
+++ Binary function.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat img = imread("C:\\Users\\duzhi\\Pictures\\Saved Pictures\\1.jpg", 0);	
+	imshow("原始图", img);
+
+	Mat img1, img2;
+
+	//大津法
+	threshold(img, img1, 100, 255, THRESH_BINARY);
+	//区域自适应二值化
+	adaptiveThreshold(img, img2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
+
+	imshow("大津法", img1);
+	imshow("区域自适应二值化", img2);
+
+	waitKey(0);
+	return 0;
+}
